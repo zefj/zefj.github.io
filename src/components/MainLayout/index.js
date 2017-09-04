a1a60d400847c7c7e3d13b883bc8c442f2555832
Filip Rec <filiprec@outlook.com>
2017-09-04 20:02:51 +0200

    Change Index to MainLayout, add social links

---
+++ src/components/MainLayout/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import GameOfLife from '../GameOfLife';
+import Well from '../Well';
+import './styles.css';
+
+class MainLayout extends Component {
+  render() {
+    return (
+      <div className="Index">
+        <GameOfLife />
+        
+        <div className="content">
+          <Well>
+            { this.props.children }
+          </Well>
+          
+          <div className="social" align="center">
+            <a href="https://www.linkedin.com/in/FilipRec"><i className="fa fa-linkedin fa-2x"></i></a>
+            <a href="https://github.com/zefj"><i className="fa fa-github fa-2x"></i></a>
+            <a href="https://www.facebook.com/naamioni"><i className="fa fa-facebook fa-2x"></i></a>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MainLayout;
