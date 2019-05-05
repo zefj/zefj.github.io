7288681144c69032e7e0786ba1951660f90a357c
Filip Rec <filiprec@outlook.com>
2019-05-05 13:39:49 +0200

    Add some comments regarding the router

--- src/router/router.js
+++ src/router/router.js
@@ -2,19 +2,23 @@ import toRegex from 'path-to-regexp';
 import React from 'react';
 
 function matchURI(path, uri) {
+  // This is roughly how this works:
+  // const pattern = toRegex('/foo/:bar', keys)
+  // // keys = [{ name: 'bar', prefix: '/', delimiter: '/', optional: false, repeat: false, pattern: '[^\\/]+?' }]
+  // // pattern = /^\/foo\/([^\/]+?)\/?$/i
   const keys = [];
   const pattern = toRegex(path, keys); // TODO: Use caching
   const match = pattern.exec(uri);
-  
+
   if (!match) return null;
 
   const params = Object.create(null);
-  
+
   for (let i = 1; i < match.length; i++) {
     params[keys[i - 1].name] =
       match[i] !== undefined ? match[i] : undefined;
   }
-  
+
   return params;
 }
 
@@ -23,9 +27,9 @@ const resolve = (routes, context) => {
     for (const route of routes) {
       const uri = context.error ? '/error' : context.pathname;
       const params = matchURI(route.path, uri);
-  
+
       if (!params) continue;
-    
+
       var result;
       if (route.index) {
         const IndexComponent = route.index;
@@ -40,6 +44,6 @@ const resolve = (routes, context) => {
     error.status = 404;
     throw error;
   });
-}
+};
 
 export default { resolve };
